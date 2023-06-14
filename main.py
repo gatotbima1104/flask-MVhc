cf8cfcfc5b25032c7a7f46a8ec673d7c34875b9e
Hanif Al Irsyad <90784532+Hanifanta@users.noreply.github.com>
2023-06-14 14:10:24 +0700

    add code deployment

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 import os
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 
+model = pickle.load(open('./model/model.pkl', 'rb'))
+vectorization = pickle.load(open('./model/vector.pkl', 'rb'))
+
+def output_label(n):
+    if n == 0:
+        return "Fake News"
+    elif n == 1:
+        return "Not a Fake News"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    news = request.json['news']
+    result = manual_testing(news)
+    
+    return jsonify(result)
+
+def manual_testing(news):
+    vect = vectorization.transform([news]).toarray()
+    pred_LR = model.predict(vect)
+
+    return {"prediction": output_label(pred_LR[0])}
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
+    return jsonify({"message": "Welcome to Fake News Detection API"})
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
